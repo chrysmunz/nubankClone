fb6490a7b17e15522beb72fec049596546ab3804
Chrystian <chrys.munz@hotmail.com>
2020-02-11 10:22:52 -0300

    first commit

---
+++ src/components/Menu/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Menu() {
+  return (
+    <Container>
+    </Container>
+  );
+}
---
+++ src/pages/Main/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { View, SafeAreaView, Text } from 'react-native';
+
+import Header from '../../components/Header';
+import Tabs from '../../components/Tabs';
+import Menu from '../../components/Menu';
+
+export default function Main() {
+    return (
+        <SafeAreaView>
+            <Text>Hello World</Text>
+        </SafeAreaView>
+        
+    );
+}
